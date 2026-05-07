43c5e397afea8b69e25f07a7a1da056b897a79f0
Pedro Augusto <pedrooknd@gmail.com>
2026-05-06 23:25:15 -0300

    feat: PalindromeNumber

---
+++ src/PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,31 @@
+package PalindromeNumber;
+
+public class PalindromeNumber {
+	public static void main(String []args) {
+		System.out.println(isPalindrome(221));
+		System.out.println(isPalindrome(212));
+
+
+	}
+	
+	 public static boolean isPalindrome(int x) {
+
+	        if (x < 0) return false;
+	        
+	        String numberString = String.valueOf(x);
+	        int left = 0;
+	        int right = numberString.length() - 1;
+	        char[] array = numberString.toCharArray();
+
+	        while(left < right){
+	            if (array[left] != array[right]) {
+	                return false;
+	            }
+	            if (array[left] == array[right]){
+	                left += 1;
+	                right -= 1;
+	            }
+	        }
+	        return true;
+	    }
+}
